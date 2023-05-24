6c1eb0c252ead3419e77196505691363f8427779
Amanda Rodrigues <rodriguesamanda49@gmail.com>
2023-05-24 17:32:19 -0300

    css ranking, feedback e novo background

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './Ranking.css';
 
 export default class Ranking extends Component {
   render() {
     const { history } = this.props;
     return (
-      <div>
+      <div className="rank-container">
         <h1 data-testid="ranking-title">Ranking</h1>
         <button
           type="button"
